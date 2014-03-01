7c645140a884e9e9175db3f1e3136780c89574db
Francis Wang <go717franciswang@hotmail.com>
2014-02-28 21:08:35 -0800

    Start ps5

---
+++ ps5/Point2D.java
@@ -0,0 +1,30 @@
+public class Point2D implements Comparable<Point2D> {
+    public Point2D(double x, double y) {
+    }
+    
+    public double x() {
+    }
+    
+    public double y() {
+    }
+    
+    public double distanceTo(Point2D that) {
+    }
+    
+    public double distanceSquaredTo(Point2D that) {
+    
+    }
+    
+    public int compareTo(Point2D that) {
+    
+    }
+    
+    public boolean equals(Object that) {
+    }
+    
+    public void draw() {
+    }
+    
+    public String toString() {
+    }
+}
---
+++ ps5/RectHV.java
@@ -0,0 +1,48 @@
+public class RectHV {
+    public RectHV(double xmin, double ymin,
+                  double xmax, double ymax) {
+    }
+    
+    public double xmin() {
+    
+    }
+    
+    public double ymin() {
+    
+    }
+    
+    public double xmax() {
+    
+    }
+    
+    public double ymax() {
+    
+    }
+    
+    public boolean contains(Point2D p) {
+    
+    }
+    
+    public boolean intersects(RectHV that) {
+    
+    }
+    
+    public double distanceTo(Point2D p) {
+    
+    }
+    
+    public double distanceSquaredTo(Point2D p) {
+    
+    }
+    
+    public boolean equals(Object that) {
+    
+    }
+    public void draw() {
+    
+    }
+    
+    public String toString() {
+    
+    }
+}
