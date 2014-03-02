44ab77d432a1bc82bba1d335e4ba7015edb04488
Francis Wang <go717franciswang@hotmail.com>
2014-03-01 19:14:14 -0800

    Start debug

--- ps5/KdTree.java
+++ ps5/KdTree.java
@@ -1,7 +1,7 @@
 import java.util.TreeSet;
 
 public class KdTree {
-    private Node parent;
+    private Node root;
     private int size;
     
     private class Node {
@@ -15,12 +15,11 @@ public class KdTree {
     }
     
     public KdTree() {
-        parent = null;
         size = 0;
     }
     
     public boolean isEmpty() {
-        return parent == null;
+        return size() == 0;
     }
     
     public int size() {
@@ -29,10 +28,11 @@ public class KdTree {
     
     public void insert(Point2D p) {
         root = insert(root, p, true);
+        size++;
     }
     
     private Node insert(Node node, Point2D p, boolean useX) {
-        if (node == null) return Node(p);
+        if (node == null) return new Node(p);
         if (useX) {
             if (node.point.x() < p.x()) {
                 return insert(node.left, p, !useX);
@@ -70,10 +70,11 @@ public class KdTree {
         return contains(node.right, p, !useX);
     }
     
-    public void draw() {Queue<Node> q = new Queue<Node>();
+    public void draw() {
+        Queue<Node> q = new Queue<Node>();
         q.enqueue(root);
         while (!q.isEmpty()) {
-            node = q.dequeue();
+            Node node = q.dequeue();
             if (node == null) continue;
             
             node.point.draw();
@@ -92,20 +93,20 @@ public class KdTree {
                        double xmin, double ymin, double xmax, double ymax) {
         if (node == null) return;
         if (rect.contains(node.point)) q.enqueue(node.point);
-        if (node.useX) {
-            if (rect.intersect(new RectHV(xmin, ymin, node.point.x(), ymax))) {
+        if (useX) {
+            if (rect.intersects(new RectHV(xmin, ymin, node.point.x(), ymax))) {
                 range(node.left, rect, q, !useX, xmin, ymin, node.point.x(), ymax);
             }
             
-            if (rect.intersect(new RectHV(node.point.x(), ymin, xmax, ymax))) {
+            if (rect.intersects(new RectHV(node.point.x(), ymin, xmax, ymax))) {
                 range(node.right, rect, q, !useX, node.point.x(), ymin, xmax, ymax);
             }
         } else {
-            if (rect.intersect(new RectHV(xmin, ymin, xmax, node.point.y()))) {
+            if (rect.intersects(new RectHV(xmin, ymin, xmax, node.point.y()))) {
                 range(node.left, rect, q, !useX, xmin, ymin, xmax, node.point.y());
             }
             
-            if (rect.intersect(new RectHV(xmin, node.point.y(), xmax, ymax))) {
+            if (rect.intersects(new RectHV(xmin, node.point.y(), xmax, ymax))) {
                 range(node.right, rect, q, !useX, xmin, node.point.y(), xmax, ymax);
             }
         }
@@ -118,7 +119,112 @@ public class KdTree {
     private Point2D nearest(Node node, Point2D p, boolean useX,
                             double xmin, double ymin, double xmax, double ymax) {
         if (node == null) return null;
-        if (node.useX) {
-            if (p.x() < 
+        double shortest = p.distanceSquaredTo(node.point);
+        Point2D p1 = node.point;
+        Point2D best = p1;
+        
+        if (useX) {
+            if (p.x() < node.point.x()) {
+                RectHV rect = new RectHV(xmin, ymin, p1.x(), ymax);
+                if (rect.distanceSquaredTo(p) < shortest) {
+                    Point2D p2 = nearest(node.left, p, !useX, xmin, ymin, p1.x(), ymax);
+                    if (p2 != null) {
+                        double d = p2.distanceSquaredTo(p);
+                        if (d < shortest) {
+                            shortest = d;
+                            best = p2;
+                        }
+                    }
+                }
+                
+                rect = new RectHV(p1.x(), ymin, xmax, ymax);
+                if (rect.distanceSquaredTo(p) < shortest) {
+                    Point2D p2 = nearest(node.right, p, !useX, p1.x(), ymin, xmax, ymax);
+                    if (p2 != null) {
+                        double d = p2.distanceSquaredTo(p);
+                        if (d < shortest) {
+                            shortest = d;
+                            best = p2;
+                        }
+                    }
+                }
+            } else {
+                RectHV rect = new RectHV(p1.x(), ymin, xmax, ymax);
+                if (rect.distanceSquaredTo(p) < shortest) {
+                    Point2D p2 = nearest(node.right, p, !useX, p1.x(), ymin, xmax, ymax);
+                    if (p2 != null) {
+                        double d = p2.distanceSquaredTo(p);
+                        if (d < shortest) {
+                            shortest = d;
+                            best = p2;
+                        }
+                    }
+                }
+                
+                rect = new RectHV(xmin, ymin, p1.x(), ymax);
+                if (rect.distanceSquaredTo(p) < shortest) {
+                    Point2D p2 = nearest(node.left, p, !useX, xmin, ymin, p1.x(), ymax);
+                    if (p2 != null) {
+                        double d = p2.distanceSquaredTo(p);
+                        if (d < shortest) {
+                            shortest = d;
+                            best = p2;
+                        }
+                    }
+                }
+            }
+        } else {
+            if (p.y() < node.point.y()) {
+                RectHV rect = new RectHV(xmin, ymin, xmax, p1.y());
+                if (rect.distanceSquaredTo(p) < shortest) {
+                    Point2D p2 = nearest(node.left, p, !useX, xmin, ymin, xmax, p1.y());
+                    if (p2 != null) {
+                        double d = p2.distanceSquaredTo(p);
+                        if (d < shortest) {
+                            shortest = d;
+                            best = p2;
+                        }
+                    }
+                }
+                
+                rect = new RectHV(xmin, p1.y(), xmax, ymax);
+                if (rect.distanceSquaredTo(p) < shortest) {
+                    Point2D p2 = nearest(node.right, p, !useX, xmin, p1.y(), xmax, ymax);
+                    if (p2 != null) {
+                        double d = p2.distanceSquaredTo(p);
+                        if (d < shortest) {
+                            shortest = d;
+                            best = p2;
+                        }
+                    }
+                }
+            } else {
+                RectHV rect = new RectHV(xmin, p1.y(), xmax, ymax);
+                if (rect.distanceSquaredTo(p) < shortest) {
+                    Point2D p2 = nearest(node.right, p, !useX, xmin, p1.y(), xmax, ymax);
+                    if (p2 != null) {
+                        double d = p2.distanceSquaredTo(p);
+                        if (d < shortest) {
+                            shortest = d;
+                            best = p2;
+                        }
+                    }
+                }
+                
+                rect = new RectHV(xmin, ymin, xmax, p1.y());
+                if (rect.distanceSquaredTo(p) < shortest) {
+                    Point2D p2 = nearest(node.left, p, !useX, xmin, ymin, xmax, p1.y());
+                    if (p2 != null) {
+                        double d = p2.distanceSquaredTo(p);
+                        if (d < shortest) {
+                            shortest = d;
+                            best = p2;
+                        }
+                    }
+                }
+            }
+        }
+            
+        return best;
     }
 }
