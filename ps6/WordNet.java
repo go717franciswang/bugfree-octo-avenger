c11fdcf294400a5181eabde2a9dbebdb36bd56a7
Francis Wang <go717franciswang@hotmail.com>
2014-03-22 19:36:12 -0700

    Start ps6

---
+++ ps6/WordNet.java
@@ -0,0 +1,19 @@
+class WordNet {
+    public WordNet(String synsets, String hypernyms) {
+    }
+    
+    public Iterable<String> nouns() {
+    }
+    
+    public boolean isNoun(String word) {
+    }
+    
+    public int distance(String nounA, String nounB) {
+    }
+    
+    public String sap(String nounA, String nounB) {
+    }
+    
+    public static void main(String[] args) {
+    }
+}
