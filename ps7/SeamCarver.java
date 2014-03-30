b644cb9656eb7e630fc56d6673780e24426ec536
Francis Wang <go717franciswang@hotmail.com>
2014-03-29 20:30:17 -0700

    Start ps7

---
+++ ps7/SeamCarver.java
@@ -0,0 +1,25 @@
+public class SeamCarver {
+    public SeamCarver(Picture picture) {
+    }
+    
+    public int width() {
+    }
+    
+    public int height() {
+    }
+    
+    public double energy(int x, int y) {
+    }
+    
+    public int[] findHorizontalSeam() {
+    }
+    
+    public int[] findVerticalSeam() {
+    }
+    
+    public void removeHorizontalSeam(int[] a) {
+    }
+    
+    public void removeVerticalSeam(int[] a) {
+    }
+}
