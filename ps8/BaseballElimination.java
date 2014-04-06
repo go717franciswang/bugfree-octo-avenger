2e6214d42fb5d8d6371fa0167208cedc7b60b0b2
Francis Wang <go717franciswang@hotmail.com>
2014-04-05 17:24:05 -0700

    Include test client

--- ps8/BaseballElimination.java
+++ ps8/BaseballElimination.java
@@ -25,4 +25,19 @@ public class BaseballElimination {
     
     public Iterable<String> certificateOfElimination(String team) {
     }
+    
+    public static void main(String[] args) {
+        BaseballElimination division = new BaseballElimination(args[0]);
+        for (String team : division.teams()) {
+            if (division.isEliminated(team)) {
+                StdOut.print(team + " is eliminated by the subset R = { ");
+                for (String t : division.certificateOfElimination(team))
+                    StdOut.print(t + " ");
+                StdOut.println("}");
+            }
+            else {
+                StdOut.println(team + " is not eliminated");
+            }
+        }
+    }
 }
