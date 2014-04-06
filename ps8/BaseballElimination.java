59e7fd53056eb0e905f443afbabe5f86c75c8567
Francis Wang <go717franciswang@hotmail.com>
2014-04-05 17:19:19 -0700

    Start ps8

---
+++ ps8/BaseballElimination.java
@@ -0,0 +1,28 @@
+public class BaseballElimination {
+    public BaseballElimination(String filename) {
+    }
+    
+    public int numberOfTeams() {
+    }
+    
+    public Iterable<String> teams() {
+    }
+    
+    public int wins(String team) {
+    }
+    
+    public int losses(String team) {
+    }
+    
+    public int remaining(String team) {
+    }
+    
+    public int against(String team1, String team2) {
+    }
+    
+    public boolean isEliminated(String team) {
+    }
+    
+    public Iterable<String> certificateOfElimination(String team) {
+    }
+}
