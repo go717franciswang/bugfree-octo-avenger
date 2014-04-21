148a6a6271ac9c7ba0f786e3adaeef6f2f32a3a4
Francis Wang <go717franciswang@hotmail.com>
2014-04-20 19:07:40 -0700

    Include test files

--- ps9/BoggleSolver.java
+++ ps9/BoggleSolver.java
@@ -8,4 +8,18 @@ public class BoggleSolver
     
     public int scoreOf(String word) {
     }
+    
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        String[] dictionary = in.readAllStrings();
+        BoggleSolver solver = new BoggleSolver(dictionary);
+        BoggleBoard board = new BoggleBoard(args[1]);
+        int score = 0;
+        for (String word : solver.getAllValidWords(board))
+        {
+            StdOut.println(word);
+            score += solver.scoreOf(word);
+        }
+        StdOut.println("Score = " + score);
+    }
 }
