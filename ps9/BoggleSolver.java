cb7f250e46644a8d739d1a5284898152b1be416d
Francis <go717franciswang@gmail.com>
2014-04-21 18:02:06 -0700

    Fill in the easy part

--- ps9/BoggleSolver.java
+++ ps9/BoggleSolver.java
@@ -1,12 +1,45 @@
 public class BoggleSolver
 {
+    private TrieSET trie;
+    
     public BoggleSolver(String[] dictionary) {
+        trie = new TrieSET();
+        for (int i = 0; i < dictionary.length; i++) {
+            String word = dictionary[i];
+            if (word.length() > 2) {
+                trie.add();
+            }
+        }
     }
     
     public Iterable<String> getAllValidWords(BoggleBoard board) {
     }
     
     public int scoreOf(String word) {
+        if (!trie.contains(word)) {
+            return 0;
+        }
+        
+        int score;
+        switch (word.length()) {
+            case 3: case 4:
+                score = 1;
+                break;
+            case 5:
+                score = 2;
+                break;
+            case 6:
+                score = 3;
+                break;
+            case 7:
+                score = 5;
+                break;
+            default:
+                score = 11;
+                break;
+        }
+        
+        return score;
     }
     
     public static void main(String[] args) {
